5747fea56f88011ca110358179ff3ce8dff4c72b
Michele Belluco <bellucomichele@gmail.com>
2017-04-04 15:08:22 +0200

    Upgrade exorcist  & uglify-js Implement minified file for all bundles file

--- example/lib/app.js
+++ example/lib/app.js
@@ -1,6 +1,9 @@
 import _ from "underscore";
+import $ from "jquery";
 
 const a = ["a", "b"];
 
 console.log("LoooL");
 console.log(a, _.isArray(a) );
+
+$('body').css({ "background": "red", "height":"350px"})
